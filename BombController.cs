60ca80143564cfb01483c2999d17b5e2539e589b
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:30:25 +0900

    Create BombController.cs
    LizardGame1.0C#

---
+++ BombController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public Transform bombPool;
+    public GameObject bombObj;
+    public GameObject boomObj;
+    private List<float> position_x = new List<float>();
+    private List<float> position_y = new List<float>();
+    private int x_size = 18;
+    private int y_size = 27;
+    private float discount = 0.1667f;
+    private int limittime = 200;
+    private int timer = 200;
+
+    private void CreateBomb()
+    {
+        int x = (int)Random.Range(0.0f, 17.9f);
+        int y = (int)Random.Range(0.0f, 26.9f);
+        GameObject bomb = Instantiate(bombObj,bombPool);
+        bomb.transform.position = new Vector2(position_x[x], position_y[y]);
+    }
+
+    void Awake()
+    {
+        for (int i = 0; i < x_size; i++)
+        {
+            position_x.Add(-1.40f + discount * i);
+        }
+        for (int i = 0; i < y_size; i++)
+        {
+            position_y.Add(-11.51f + discount * i);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (PlayerController.lebel >= 4)
+        {
+            if (timer > limittime)
+            {
+                CreateBomb();
+                CreateBomb();
+                timer = 0;
+            }
+            else
+            {
+                timer++;
+            }
+        }
+    }
+}
