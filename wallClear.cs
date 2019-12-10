fd2fa7c076d3ac7f6b8fa0afc90024b84f31c678
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:31:20 +0900

    Create wallClear.cs
    LizardGame1.0C#

---
+++ wallClear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallClear : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Wall")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 fixedposition = new Vector2(0.008f, -7.1f);
+        transform.position = Vector2.MoveTowards(transform.position, fixedposition, Time.deltaTime*6);
+        if(transform.position==fixedposition||PlayerController.gameover)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
