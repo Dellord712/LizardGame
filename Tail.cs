ca4ccac41120c23c22fd4d16e8a5b7f48b77f029
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:31:12 +0900

    Create Tail.cs
    LizardGame1.0C#

---
+++ Tail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    public static bool destroyTail = false;
+    public Sprite infection;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            PlayerController.hp = 0f;
+        }
+        if (collision.gameObject.tag == "Ghost"&&transform.gameObject.tag=="Tail")
+        {
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = infection;
+            transform.gameObject.tag = "Infection";
+            collision.gameObject.SetActive(false);
+            GhostController.setghost = false;
+            PlayerController.tailCount = 0;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (PlayerController.gameover||destroyTail)
+        {
+            Destroy(gameObject);
+            destroyTail = false;
+        }
+    }
+}
