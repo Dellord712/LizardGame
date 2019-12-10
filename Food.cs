75a3b5a955edf942cce516f4439ce22ac6c8f621
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:30:42 +0900

    Create Food.cs
    LizardGame1.0C#

---
+++ Food.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public static bool destroyFood = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            PlayerController.hp += 17f;
+            PlayerController.createtail = true;
+            if (gameObject.tag == "Cake")
+            {
+                PlayerController.createtail2 = true;
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (PlayerController.gameover||destroyFood)
+        {
+            Destroy(gameObject);
+            destroyFood = false;
+        }
+    }
+}
