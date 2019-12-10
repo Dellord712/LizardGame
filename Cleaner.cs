51c3d5f60a1dbbcdd5b72a1ef5664ebb68ff1170
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:30:32 +0900

    Create Cleaner.cs
    LizardGame1.0C#

---
+++ Cleaner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleaner : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            CleanerController.cleanerCount++;
+            Destroy(transform.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(PlayerController.gameover)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
