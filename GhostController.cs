237b1e1e9c821e43c34faa4384f70943d76ac504
김승일 <43162253+Dellord712@users.noreply.github.com>
2019-12-11 02:30:54 +0900

    Create GhostController.cs
    LizardGame1.0C#

---
+++ GhostController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    public static bool setghost = false;
+    public Transform ghost;
+    private int count = 290;
+    private int time = 300;
+
+    private void Awake()
+    {
+        ghost.gameObject.SetActive(false);
+    }
+    private void FixedUpdate()
+    {
+        if (PlayerController.lebel < 5)
+        {
+            setghost = false;
+            count = 290;
+        }
+        if (PlayerController.lebel >= 5&&!setghost)
+        {
+            if (count > time)
+            {
+                ghost.gameObject.SetActive(true);
+                setghost = true;
+                ghost.position = new Vector2(-1.4f,-6.98f);
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+}
